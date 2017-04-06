426daa73549bec76c7f50f7b25361b7477b46a03
Chris Watt <cwatt@mail.umw.edu>
2017-04-05 22:09:01 -0400

    added java doc comments to ItemSpecificCommand

--- ItemSpecificCommand.java
+++ ItemSpecificCommand.java
@@ -6,19 +6,27 @@
 package borkv3;
 
 /**
- *
+ * runs commands specific to items
  * @author qureshi225
  */
 public class ItemSpecificCommand extends Command {
 
     private String verb;
     private String noun;
-
+    /**
+     * assigns verb and noun to their specific variables
+     * @param verb
+     * @param noun 
+     */
     public ItemSpecificCommand(String verb, String noun) {
         this.verb = verb;
         this.noun = noun;
     }
-
+    /**
+     * checks if you have the noun or if it is in the same room as you and then checks if you
+     * can verb the noun
+     * @return message telling you if you did something with the noun or not
+     */
     public String execute() {
         GameState state = GameState.Instance();
         if (state.getDungeon().itemCommands.contains(verb)) {
