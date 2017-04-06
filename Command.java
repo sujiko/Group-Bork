6913db4aa79881904a98a91836f35bc26fbce73e
Chris Watt <cwatt@mail.umw.edu>
2017-04-05 20:05:52 -0400

    Added java doc to Command

--- Command.java
+++ Command.java
@@ -7,11 +7,16 @@ package borkv3;
 
 
 /**
- *
+ * @param nothing
+ * @return a string from the execute command
+ * @throws nothing
  * @author qureshi225
  */
 abstract class Command {
-
+    /**
+     * 
+     * @return a string describing what you did or where you went
+     */
     abstract String execute();
 }
 
