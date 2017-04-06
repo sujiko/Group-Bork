689843b83d59ace3d34a15889153d581c414febb
Chris Watt <cwatt@mail.umw.edu>
2017-04-05 23:30:35 -0400

    created a shell of the combat class in order to calculate damage between the user, monsters, and your follower

---
+++ Combat.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GroupBorkv1.borkv3;
+
+/**
+ *this class will handle calculating how much damage you, your follower, and monsters will take
+ * @author christian
+ */
+public class Combat {
+     
+    /**
+     * will calculate how much damage the monster inflicts on the user
+     * @return damage to user
+     */
+    public int monVuser(){
+        return 0;
+    }
+    
+    /**
+     * will calculate how much damage the monster inflicts on the follower
+     * @return damage to follower
+     */
+    public int monVfollower(){
+        return 0;
+    }
+    
+    /**
+     * will calculate how much damage you do towards the monster
+     * @return damage to monster
+     */
+    public int userVmon(){
+        return 0;
+    }
+    
+    /**
+     * will calculate how much damage your follower inflicts on the monster
+     * @return damage to monster
+     */
+    public int followerVmon(){
+        return 0;
+    }
+    
+}
