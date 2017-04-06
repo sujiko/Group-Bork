99aa69affb0060df984c246177dfe6f71c20ed4a
Chris Watt <cwatt@mail.umw.edu>
2017-04-05 22:55:54 -0400

    created a shell of the attack command for the user and added java doc comments

---
+++ AttackCommand.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GroupBorkv1.borkv3;
+
+/**
+ * the attack command will allow the player to attack monsters
+ * @author christian
+ */
+public class AttackCommand extends Command{
+    String monsterName;
+    /**
+     * when run the AttackCommand will assign the name of the monster to the variable monsterName
+     * @param noun 
+     */
+    public AttackCommand(String noun){
+        this.monsterName=noun;
+    }
+    
+    /**
+     * when run the execute will check to see if the monster is in the current room.
+     * if a monster is in the room it will calculate your damage to the monster and then
+     * tell you if you killed it or not and what rewards you get
+     * @return 
+     */
+    public String execute(){
+        return"";
+    }
+}
