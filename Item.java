0d87074132c81daeab291b53814366423ac887b5
sujiko <qureshi.ak225@gmail.com>
2017-04-05 22:31:14 -0400

    added javadoc to the items class

--- Item.java
+++ Item.java
@@ -11,7 +11,7 @@ import java.util.Hashtable;
 import java.util.Set;
 
 /**
- *
+ *this class takes care of all the items the game takes in
  * @author qureshi225
  */
 public class Item {
@@ -37,26 +37,48 @@ public class Item {
 
         }
     }
-
+/**
+*checks to see if the item in question goes by the name that is trying to be used.
+*@param name
+*          the string you are trying to compare the item name too 
+*@return by default returns true unless there are other names an item can go by
+*/
     public boolean goesBy(String name) {
         return true;
     }
-
+/**
+* the name the item is connected to in the hashtable
+*@return the string that is the name of the item.
+*/
     public String getPrimaryName() {
         return primaryName;
     }
-
+/**
+*this gets the message based on the verb contained int he hashtable
+*@param verb
+*           the verb given to search for the message of
+*@return the messaged connected to the verb
+*/
     public String getMessageForVerb(String verb) {
         return messages.getOrDefault(verb, null);
     }
-
+/**
+*gives the interget weight of the item(the so called score)
+*@return the weight of the object
+*/
     public int getWeight() {
         return this.weight;
     }
+    /**
+    * gets the list of verbs out of the hashtable for messages
+    *@return a set of the verbs.
+    */
     public Set<String> getKeys(){
         return messages.keySet();
     }
-
+/**
+* the unused tostring of the item.
+*/
     public String toString() {
         return "";
     }
