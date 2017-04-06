d3d98669d7173b5b99dbbcca6fb2a1cfb1ddcd23
Chris Watt <cwatt@mail.umw.edu>
2017-04-05 22:24:24 -0400

    added java doc comments to UnknownCommand

--- UnknownCommand.java
+++ UnknownCommand.java
@@ -6,15 +6,22 @@
 package borkv3;
 
 /**
- *
+ * returns a message that lets you know that the system doesn't understand your command
  * @author qureshi225
  */
 public class UnknownCommand extends Command {
     String bogusCommand;
-    
+    /**
+     * assigns your command the system doesn't recognize to the variable bogusCommand
+     * @param bc 
+     */
     public UnknownCommand(String bc){
         bogusCommand=bc;
     }
+    /**
+     * runs when the system doesn't understand your command
+     * @return returns a message that lets you know that the system doesn't understand your command
+     */
     public String execute(){
         return "You can't do that..... but you tried! A for effort.";
     }
