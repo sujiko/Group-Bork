7947471d9696d72b7774e413a47bb7fcc9868f1c
sujiko <qureshi.ak225@gmail.com>
2017-04-05 22:49:08 -0400

    created a price command for the shopkeeper and player.. DON'T DO THIS AGAIN GITHUB. STOP YELLING AT ME.

---
+++ PriceCommand.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package borkv3;
+
+/**
+ *the price command will allow a player to see the price of an item in  a shopkeepers inventory
+ * @author qureshi225
+ */
+public class PriceCommand extends Command {
+    String itemName;
+    public PriceCommand(String itemName){
+        this.itemName=itemName;
+    }
+    /**
+    * this will execute the command interaction between a shopkeeper and player
+    *@return the string output to the player.
+    */
+    public String execute(){
+       return ""
+     }
+}
