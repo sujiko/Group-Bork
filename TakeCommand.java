6dc5f21a7e20a6392a86d9049be86e701693de78
Chris Watt <cwatt@mail.umw.edu>
2017-04-05 21:12:03 -0400

    Added java doc to TakeCommand and its methods

--- TakeCommand.java
+++ TakeCommand.java
@@ -6,15 +6,24 @@
 package borkv3;
 
 /**
- *
+ * TakeCommand is an Extension of the Command class and specializes on adding items to your inventory
  * @author qureshi225
+ * @throws NoItemException
  */
 public class TakeCommand extends Command {
     String itemName;
-    
+    /**
+     * TakeCommand takes the name of the itemName and sets it in the variable itemName
+     * @param itemName 
+     */
     public TakeCommand(String itemName){
         this.itemName=itemName;   
     }
+    /**
+     * @return a string that tells you if you have taken the item or
+     * if it isn't in the room
+     * @throws NoItemException
+     */
     public String execute(){
         if(itemName.equals("")){
             return "Take what?";
