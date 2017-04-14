6108def52f01483b3d61f77d6bd6a01e8abce524
Qures <Qures@10.80.49.109>
2017-04-14 11:12:05 -0400

    added TransformEvent class

---
+++ TransformEvent.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GroupBork;
+
+/**
+ *
+ * @author Qures
+ */
+public class TransformEvent extends Event {
+    private Item item;
+    private String actedUpon;
+    
+    public TransformEvent(String actedUpon, Item i){
+        this.item=i;
+        this.actedUpon= actedUpon;
+    }
+    
+    public void execute(){
+        GameState in= GameState.Instance();
+           if(in.getInventoryNames().contains(item.getPrimaryName())){
+            in.removeFrominventory(item);
+        }else if(in.getAdvenurersCurrentRoom().contains(item)){
+            in.getAdvenurersCurrentRoom().remove(item);
+        }
+           in.addToInventory(in.getDungeon().getItem(actedUpon));       
+    }
+}
