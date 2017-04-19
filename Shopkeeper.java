f51c284f220a1392308bd49521c473401030395f
Qures <Qures@10.80.49.109>
2017-04-19 12:29:12 -0400

    started creation of shopkeeper to a monster

--- Shopkeeper.java
+++ Shopkeeper.java
@@ -10,55 +10,71 @@ import java.util.Hashtable;
 import java.util.Random;
 
 /**
- *this class holds a shopkeeper if there is one for the bork game.
+ * this class holds a shopkeeper if there is one for the bork game.
+ *
  * @author qureshi225
  */
-public class Shopkeeper{
-  private ArrayList<Item> toSell= new ArrayList<Item>();
-  private final int HEALTH= 9001;
-  
-  
-  /**
-  * this method removes items from the shopkeeper inventory 
-  *@param i
-  *       this is the item that you are adding to the inventory
-  */
-    public void removeItem(Item i){
+public class Shopkeeper extends Monster{
+
+    private ArrayList<Item> toSell = new ArrayList<Item>();
+    private String name;
+    private final int LIFE = 9001;
+    private int attkPWR;
+    boolean hostile = false;
+    
+    /**
+     *
+     * @param name
+     */
+    public Shopkeeper(String name){
+        super(name);
+        this.name=name;
+    }
+
+    /**
+     * this method removes items from the shopkeeper inventory
+     *
+     * @param i this is the item that you are adding to the inventory
+     */
+    public void removeItem(Item i) {
         toSell.remove(i);
     }
-  
-  /**
-  * this method will add items to the shopkeeper inventory
-  *@param i
-  *      the item to be added
-  */
-    public void addItem(Item i){
+
+    /**
+     * this method will add items to the shopkeeper inventory
+     *
+     * @param i the item to be added
+     */
+    public void addItem(Item i) {
         toSell.add(i);
     }
-  
-  /**
-  * this method will givey ou the price of the item you ask the shopkeeper for
-  *@param name
-  *         name of the item you want the price of
-  *@return the price of the item
-  */
-    public int price(Item i){
-        int price= i.getScore();
+
+    /**
+     * this method will givey ou the price of the item you ask the shopkeeper
+     * for
+     *
+     * @param name name of the item you want the price of
+     * @return the price of the item
+     */
+    public int price(Item i) {
+        int price = i.getScore();
         return price;
     }
+
     /**
-     * this method will tell you everything the shopkeeper is currently selling. 
-     * @param currentInventorySeed
-     *                  the interger seed of for the currently displayed inventory.
+     * this method will tell you everything the shopkeeper is currently selling.
+     *
+     * @param currentInventorySeed the interger seed of for the currently
+     * displayed inventory.
      */
-    public void printSelling(int currentInventorySeed){
-        Random rng= new Random(currentInventorySeed);
-        int itemOne= rng.nextInt(toSell.size());
-        int itemTwo= rng.nextInt(toSell.size());
-        int itemThree= rng.nextInt(toSell.size());
-        int itemFour= rng.nextInt(toSell.size());
-        System.out.println("I'm selling: "+toSell.get(itemOne).getPrimaryName()+ ", "+toSell.get(itemTwo).getPrimaryName()+ ", "
-                +toSell.get(itemThree).getPrimaryName()+ ", "+toSell.get(itemFour).getPrimaryName()+".");
-        
+    public void printSelling(int currentInventorySeed) {
+        Random rng = new Random(currentInventorySeed);
+        int itemOne = rng.nextInt(toSell.size());
+        int itemTwo = rng.nextInt(toSell.size());
+        int itemThree = rng.nextInt(toSell.size());
+        int itemFour = rng.nextInt(toSell.size());
+        System.out.println("I'm selling: " + toSell.get(itemOne).getPrimaryName() + ", " + toSell.get(itemTwo).getPrimaryName() + ", "
+                + toSell.get(itemThree).getPrimaryName() + ", " + toSell.get(itemFour).getPrimaryName() + ".");
+
     }
 }
