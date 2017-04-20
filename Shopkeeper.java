97080144af019583de80831c460786562c8983fa
Chris Watt <cwatt@mail.umw.edu>
2017-04-19 22:59:50 -0400

    added methods from monster. also overrided several other methods.

--- Shopkeeper.java
+++ Shopkeeper.java
@@ -17,18 +17,18 @@ import java.util.Random;
 public class Shopkeeper extends Monster{
 
     private ArrayList<Item> toSell = new ArrayList<Item>();
-    private String name;
-    private final int LIFE = 9001;
+    private String monName;
+    private int life = 9001;
     private int attkPWR;
     boolean hostile = false;
-    
+    private ArrayList<Item> lootItems = new ArrayList<Item>();
     /**
      *
      * @param name
      */
     public Shopkeeper(String name){
         super(name);
-        this.name=name;
+        this.monName=name;
     }
 
     /**
@@ -77,4 +77,79 @@ public class Shopkeeper extends Monster{
                 + toSell.get(itemThree).getPrimaryName() + ", " + toSell.get(itemFour).getPrimaryName() + ".");
 
     }
+        /**
+     * tells you the monsters name
+     * @return monsters name
+     */
+    public String getMonName(){
+        return this.monName;
+    }
+    /**
+     * tells you the monsters attack power
+     * @return monsters attack power
+     */
+    public int getAttack(){
+        return this.attkPWR;
+    }
+    /**
+     * this will be used to scale the monsters attack according to the users level
+     */
+    @Override
+    public void setAttack(){
+        this.attkPWR = GameState.Instance().getStrength() /4;
+    }
+    /**
+     * this will return the monsters life
+     * @return monsters amount of health
+     */
+    public int getLife(){
+        return this.life;
+    }
+    /**
+     * allows monster to take damage
+     * @param dmg 
+     */
+    public void takeDMG(int dmg){
+        this.life -=dmg;
+    }
+    /**
+     * this will be used to scale the monsters health according to the users level
+     */
+    @Override
+    public void setLife(){
+        this.life=9001; 
+    }
+    /**
+     * this will be used to get the loot the monster holds when it is killed
+     * an arraylist of loot that will be dropped when the monsters health hits 0
+     */
+    public void getLoot(){
+        if (lootItems.isEmpty()){
+        }else
+            for(Item item:lootItems){
+            GameState.Instance().addToInventory(item);
+        }
+        
+    }
+    
+    
+    @Override
+    public void genLoot(){
+        ArrayList<Item> possibleLoot =GameState.Instance().getDungeon().getItemsList();
+        int gen = (int)(Math.random()*3)+1;
+        for(int x = 1; x<gen; x++){
+            int lootItem = (int)(Math.random()* possibleLoot.size()-1);
+            this.lootItems.add(possibleLoot.get(lootItem));
+        }
+    }
+    /**
+     * this will be used to manually set the monsters hostility to true.
+     */
+    public void setHostileT(){
+        this.hostile=true;
+    }
+    public boolean getHostility(){
+        return this.hostile;
+    }
+    
 }
