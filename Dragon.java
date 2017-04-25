b5e2c5cadf09f46e8f95e3b98424fcbf88d304a5
Chris <Chris@10.213.144.164>
2017-04-24 23:35:55 -0400

    edited Dragon

--- Dragon.java
+++ Dragon.java
@@ -5,17 +5,54 @@
  */
 package GroupBork;
 
+import java.util.ArrayList;
+
 /**
  *  Initializes singleton "Dragon" which extends the class monster
  * 
  * @author Elliot
  */
-public class Dragon extends Monster
-{
-    private static final Dragon theDragon = new Dragon("Dragon");
-
-    private Dragon(String name) 
-    {
+public class Dragon extends Monster{
+    private static Shopkeeper onlyInstance;
+    public static String monName;
+    private final int life = 1;
+    private final int attkPWR=0;
+    private Room currentRoom;
+    private boolean dead = false;
+    private ArrayList<Item> lootItems = new ArrayList<Item>();
+    Dragon(String name) {
         super(name);
+        this.monName = "chibiUsa";
+    }
+
+    public static synchronized Shopkeeper Instance() {
+        if (onlyInstance == null) {
+            onlyInstance = new Shopkeeper(monName);
+        }
+        return onlyInstance;
+    }
+    
+    public void setRoom(Room room) {
+        this.currentRoom = room;
+    }
+    @Override
+    public void genLoot(){
+    }
+    @Override
+    public void getLoot(){
+        died();
+    }
+    public Room getRoom() {
+        return this.currentRoom;
+    }
+    public boolean dead() {
+        return this.dead;
+    }
+
+    public void died() {
+        this.dead = true;
+        DieEvent a = new DieEvent();
+        a.execute();
+        System.out.println("You killed the dragon. you were supposed to save it. you lose");
     }
 }
